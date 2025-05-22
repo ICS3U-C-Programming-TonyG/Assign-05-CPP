b02ef484157c7bfbd000a3fecbb5652a22a056cb
TONY GAO <TONY.GAO@STU.OCSB.CA>
2025-05-22 20:49:57 +0000

    2

---
+++ caesarCipherEncryper.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+std::string encrypt_text(const std::string plaintext, int n) {
+    std::string ans = "";
+    for (char ch : plaintext) {
+        // Compound Boolean and nested if
+        if ((isalpha(ch) && !isdigit(ch)) || ch == ' ') {
+            if (ch == ' ') {
+                ans += ' ';
+            } else {
+                if (isupper(ch)) {
+                    ans += (char)((((ch - 'A') + n) % 26) + 'A');
+                } else {
+                    ans += (char)((((ch - 'a') + n) % 26) + 'a');
+                }
+            }
+        } else {
+            ans += ch;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    std::string plaintext;
+    int n;
+
+    std::cout << "Write your message: ";
+    std::getline(std::cin, plaintext);
+    std::cout << "What order of magnitude? ";
+    std::cin >> n;
+    std::cout << "Plain Text is : " << plaintext << std::endl;
+    std::cout << "Shift pattern is : " << n << std::endl;
+    std::cout << "Cipher Text is : " << encrypt_text(plaintext, n) << std::endl;
+}
