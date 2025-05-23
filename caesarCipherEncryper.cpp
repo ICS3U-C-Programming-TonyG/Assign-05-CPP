1d9423623b3c2c3844cc7a4a2bee85ff0789935a
TONY GAO <TONY.GAO@STU.OCSB.CA>
2025-05-23 01:28:42 +0000

    hi

--- caesarCipherEncryper.cpp
+++ caesarCipherEncryper.cpp
@@ -1,7 +1,13 @@
+// Copyright (c) 2020 Tony G All rights reserved.
+//
+// Created by: Tony G
+// Created on: 2025-03-17
+// Caesar Cipher encryption.
+
 #include <iostream>
 #include <string>
 
-std::string encrypt_text(const std::string plaintext, int n) {
+std::string encrypt_text(const std::string& plaintext, int n) {
     std::string ans = "";
     for (char ch : plaintext) {
         // Compound Boolean and nested if
@@ -10,9 +16,9 @@ std::string encrypt_text(const std::string plaintext, int n) {
                 ans += ' ';
             } else {
                 if (isupper(ch)) {
-                    ans += (char)((((ch - 'A') + n) % 26) + 'A');
+                    ans += static_cast<char>((((ch - 'A') + n) % 26) + 'A');
                 } else {
-                    ans += (char)((((ch - 'a') + n) % 26) + 'a');
+                    ans += static_cast<char>((((ch - 'a') + n) % 26) + 'a');
                 }
             }
         } else {
@@ -28,9 +34,13 @@ int main() {
 
     std::cout << "Write your message: ";
     std::getline(std::cin, plaintext);
+
     std::cout << "What order of magnitude? ";
     std::cin >> n;
-    std::cout << "Plain Text is : " << plaintext << std::endl;
-    std::cout << "Shift pattern is : " << n << std::endl;
-    std::cout << "Cipher Text is : " << encrypt_text(plaintext, n) << std::endl;
+
+    std::cout << "Plain Text is: " << plaintext << std::endl;
+    std::cout << "Shift pattern is: " << n << std::endl;
+    std::cout << "Cipher Text is: " << encrypt_text(plaintext, n) << std::endl;
+
+    return 0;
 }
